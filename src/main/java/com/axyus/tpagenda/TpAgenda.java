c732715675f03e04cbe8666f67cdc1450a9979d6
Farah GAUDUIN <farah.gauduin@axyus.local.axyus.com>
2023-01-16 17:37:16 +0100

    using axyus jdbc

--- src/main/java/com/axyus/tpagenda/TpAgenda.java
+++ src/main/java/com/axyus/tpagenda/TpAgenda.java
@@ -1,4 +1,5 @@
 package com.axyus.tpagenda;
+
 import com.axyus.tpagenda.bll.AgendaManager;
 import com.axyus.tpagenda.bo.Address;
 import com.axyus.tpagenda.bo.Customer;
@@ -16,32 +17,15 @@ public class TpAgenda {
         AgendaManager agendaManager = new AgendaManager();
         try {
             utils.initialize();
-            
+
             //get and find
             agendaManager.getAllCustomers();
             agendaManager.getAllAddresses();
-            agendaManager.findAddressById(1);
-            agendaManager.findCustomerbyAddressId(1);
-            
-            //add new address
-            Address address = new Address(6,"Boulevard des rêves", "Wonderland", "26500", "Neverland");
-            int idAddress1 = agendaManager.insertAddress(address);
-            
-            //add new customer
-            Customer customer = new Customer("Reynolds", "Dan", "ImagineDragons", "imagine@fragons.com", 06323232, 3);
-            int idCustomer1 = agendaManager.insertCustomer(customer);
-            
-            //delete
-            agendaManager.deleteAddress(idAddress1);
-            agendaManager.deleteCustomer(idCustomer1);
-            
-            //update
-            agendaManager.updateUsername(1, "farahaha");
-            
+
         } catch (IOException ex) {
             Logger.getLogger(TpAgenda.class.getName()).log(Level.SEVERE, null, ex);
         }
 
     }
-    
+
 }
